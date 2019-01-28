fca1949c88ed538cf4803ba23aa6e5b7347d0674
Ali <aljabri55@gmail.com>
2019-01-28 13:06:53 +0300

    initial commit

---
+++ utilites/database_Utilities.py
@@ -0,0 +1,6 @@
+def creat_database(database_path:str):
+    #TODO: generate the database
+    pass
+def save_word_to_database(database_path:str, word_list:list):
+    #TODO: save the words to the database
+    pass
---
+++ utilites/url_utilities.py
@@ -0,0 +1,9 @@
+def load_urls_from_file(file_path:str):
+    #todo: add the code needed to read the text file with urls in it
+    pass
+def load_page(url:str):
+    #todo: add the code that reads the url
+    pass
+def scrape_page(page_contenent):
+    #todo: Analyez the text
+    pass
